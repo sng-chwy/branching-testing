dc83284186ebf773e58976e4efb09b5f87a93674
Sidney Ng <sng@chewy.com>
2020-06-02 13:48:43 -0400

    hotfix3

--- simple.sh
+++ simple.sh
@@ -2,5 +2,7 @@ echo "Hello, world!!!!!"
 echo "We need a fix ASAP!"
 echo "Wow what a nice feature"
 echo "The world is burning! FIX FIX FIX"
+echo "Oh no not again :("
 echo "So many new features"
 echo "feature3"
+
